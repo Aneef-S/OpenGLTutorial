2573438beb22eba054f08cc2437e0833da4df6e3
Aneef-S <aneefchulu205@gmail.com>
2026-02-10 15:48:11 +0530

    Getting Started - Coordinate Systems

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 unsigned int GetTextureFromImage(const std::string &imagePath);
 
+constexpr int WINDOW_WIDTH = 800;
+constexpr int WINDOW_HEIGHT = 600;
 
 int main()
 {
@@ -25,7 +27,12 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(
+			WINDOW_WIDTH,
+			WINDOW_HEIGHT,
+			"LearnOpenGL",
+			NULL,
+			NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -44,43 +51,70 @@ int main()
 
 	//-----------------------------------Shader Compilation---------------------------------------------//
 
-	const char *vertexShaderPath = "Shaders/vertexShader.glsl";
-	const char *fragmentShaderPath = "Shaders/fragmentShader.glsl";
+	constexpr const char *vertexShaderPath = "Shaders/vertexShader.glsl";
+	constexpr const char *fragmentShaderPath = "Shaders/fragmentShader.glsl";
 
 	Shader shader = Shader(vertexShaderPath, fragmentShaderPath);
 
 	// Textures
 
-	const char *textureImagePath = "Resources/container.jpg";
+	constexpr const char *textureImagePath = "Resources/container.jpg";
 	unsigned int containerTextureID = GetTextureFromImage(textureImagePath);
 	unsigned int smilyTextureID = GetTextureFromImage("Resources/awesomeface.png");
-	
-	
 
 	//------------------ Set up vertex data and buffers and configure vertex attributes ------------------//
 
 	// Components
 	float vertices[] = {
-			// positions          // colors           // texture coords
-			0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,		// top right
-			0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,	// bottom right
-			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-			-0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f		// top left
-	};
+			-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+			0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 
-	unsigned int indices[] = {
-			0, 1, 2,
-			0, 3, 2
-		};
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+			0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+			-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+			-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+			-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+			-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+			0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+			0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+			-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+			0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+			0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+			-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
 
 	// Vertex Buffer Object
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 
 	// Generate Vertex Array Object ID
 	glGenVertexArrays(1, &VAO);
 	// Generate buffer ID
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	// Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
@@ -90,33 +124,33 @@ int main()
 	// Copy vertices data to buffer's memory
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 	// Specify how OpenGL should interpret the vertex data
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+	glVertexAttribPointer(
+			0,								 // Index
+			3,								 // Number of point for this attributes
+			GL_FLOAT,					 // Type
+			GL_FALSE,					 // Need to normalize
+			5 * sizeof(float), // Stride/ Total poinst of this vertex
+			(void *)0					 // Posistion to this attibute wrt the stride.
+	);
 	glEnableVertexAttribArray(0);
 
-	// Setting color
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
 	// Setting texture coordinates.
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glEnable(GL_DEPTH_TEST);
 
 	shader.Use();
 	shader.SetInt("containerTexture", 0);
-	shader.SetInt("smilyTexture",1);
+	shader.SetInt("smilyTexture", 1);
 
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, containerTextureID);
 
 	glActiveTexture(GL_TEXTURE1);
-	glBindTexture(GL_TEXTURE_2D,smilyTextureID);
+	glBindTexture(GL_TEXTURE_2D, smilyTextureID);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -130,20 +164,28 @@ int main()
 		glClearColor(0.1f, 0.1f, 0.4f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		
+		// Matrix to convert object from local sapce to world space.
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::rotate(model, currentTime, glm::vec3(0.0f, 1.0f, 1.0f));
 
-		glm::mat4 trans = glm::mat4(1.0f);
-		trans = glm::rotate(trans, currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
-		trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-		float sinOfTime = sin(currentTime);
-		trans = glm::scale(trans,glm::vec3(sinOfTime,sinOfTime,1.0f));
+		// Matrix to convert from world space to view/camera space.
+		glm::mat4 view = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+		glm::mat4 projection = glm::perspective(
+				glm::radians(45.0f),												// FOV
+				(float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, // Aspect ratio
+				0.1f,																				// near plane
+				100.0f																			// far plane
+		);
 
 		shader.Use();
-		shader.SetMatrix4fv("transform",trans);
-
+		shader.SetMatrix4fv("model", model);
+		shader.SetMatrix4fv("view",view);
+		shader.SetMatrix4fv("projection",projection);
 
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
@@ -152,7 +194,6 @@ int main()
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 
 	return 0;
@@ -173,7 +214,7 @@ unsigned int GetTextureFromImage(const std::string &imagePath)
 {
 	unsigned int textureID;
 	glGenTextures(1, &textureID);
-	glBindTexture(GL_TEXTURE_2D, textureID);	
+	glBindTexture(GL_TEXTURE_2D, textureID);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -188,20 +229,23 @@ unsigned int GetTextureFromImage(const std::string &imagePath)
 	if (textureData)
 	{
 		GLenum format;
-		if (nrChannles == 1) format = GL_RED;
-		else if (nrChannles == 3) format = GL_RGB;
-		else if (nrChannles == 4) format = GL_RGBA;
+		if (nrChannles == 1)
+			format = GL_RED;
+		else if (nrChannles == 3)
+			format = GL_RGB;
+		else if (nrChannles == 4)
+			format = GL_RGBA;
 
 		glTexImage2D(
-			GL_TEXTURE_2D,
-			0, 
-			format, 
-			width, 
-			height, 
-			0, 
-			format, 
-			GL_UNSIGNED_BYTE, 
-			textureData);
+				GL_TEXTURE_2D,
+				0,
+				format,
+				width,
+				height,
+				0,
+				format,
+				GL_UNSIGNED_BYTE,
+				textureData);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 	else
