34ebcf499fbe95f56689cb6f36aaead9e7eda186
Aneef-S <aneefchulu205@gmail.com>
2026-02-10 19:16:28 +0530

    Added Camera Class

---
+++ src/Headers/Camera.h
@@ -0,0 +1,59 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+enum Camera_Movements{
+  FORWARD,
+  BACKWARD,
+  LEFT,
+  RIGHT
+};
+
+constexpr float YAW = -90.0f;
+constexpr float PITCH = 0.0f;
+constexpr float SPEED = 2.5f;
+constexpr float SENSITIVITY = 0.1f;
+constexpr float ZOOM = 45.0f;
+
+
+class Camera
+{
+public:
+  glm::vec3 Position;
+  glm::vec3 Up;
+  glm::vec3 Front;
+  glm::vec3 Right;
+  glm::vec3 WorldUp;
+  // euler Angles
+  float Yaw;
+  float Pitch;
+  // camera options
+  float MovementSpeed;
+  float MouseSensitivity;
+  float Zoom;
+
+  Camera(glm::vec3 position = glm::vec3(0.0f), glm::vec3 up = glm::vec3(0.0f,0.0f,-1.0f),float yaw = YAW,float pitch = PITCH): 
+    Position(position),
+    WorldUp(up),
+    Yaw(yaw),
+    Pitch(pitch)
+  { 
+    UpdateCameraVectors();
+  }
+
+  glm::mat4 GetViewMatrix();
+  void ProcessKeyboardInput(Camera_Movements direcion,float deltaTime);
+  void ProcessMouseMovement(float xoffset,float yoffset,GLboolean constrainPitch = true);
+  void ProcessMouseScroll(float yoffset);
+
+private:
+  void UpdateCameraVectors();
+
+}; 
+
+
+
+#endif 
